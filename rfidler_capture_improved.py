527398a149a54acead2df5a8fb4382ad2495ac81
dreilly369 <executivewd@hotmail.com>
2016-08-21 19:51:19 -0700

    adding code to look for button if RPi.GPIO package is present (#7)

--- rfidler_capture_improved.py
+++ rfidler_capture_improved.py
@@ -4,7 +4,8 @@ from time import sleep
 import RFIDler
 from optparse import OptionParser
 from random import randint
-# This script is used to try and guess an unknown RFID tags UID. 
+
+# This script is used to try and guess an unknown RFID tag's UID. 
 
 def dict_output(input, delimiter=":"):
     out = {}
@@ -50,13 +51,13 @@ parser = OptionParser()
 parser.add_option("-c", "--card-type", dest="cardType", default ="askraw",
                   help="The type of card to emulate")
 parser.add_option("-r", "--replay-file", dest="replayFile", default=None,
-                      help="try making clones from a previous capture file")
+                      help="try making clones from a previous capture file [WIP]")
 parser.add_option("-o", "--out-file", dest="outFile", default="quick_capture_%d.txt" % randint(0,999999999999999),
                       help="The first id in the series to try")
 parser.add_option("-p", "--port", dest="commPort", default="/dev/ttyACM0",
                       help="The number of samples per chunk")
 parser.add_option("-m", "--multi-clone", dest="multiClone", default=False,
-                      help="Offers to clone each card type with an unempty value")
+                      help="Offers to clone each card type with an unempty value. DO NOT USE WITH -b (--button-poll)")
 
                       
 (options, args) = parser.parse_args()
@@ -70,54 +71,59 @@ except ImportError:
     print("Error importing RPi.GPIO! Skipping GPIO environment. If you are on a Pi make sure to run with sudo \n")
     on_pi = False
     
-if on_pi:
-    mode = GPIO.getmode()
-    capture_btn = 24 # Bring to ground to start a capture stream
-    working_led = 23
-    print "Pin number mode: %s" % mode
-    GPIO.setup(working_led, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.setup(capture_btn, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
 port = options.commPort
 rfidler = RFIDler.RFIDler()
 result, reason = rfidler.connect(port)
 if not result:
     print 'Warning - could not open serial port:', reason
     exit()
-result, data=rfidler.command('autotag ')
-detected = {}
-with open(options.outFile,'w') as f:
-    for c in data:
-        if c.strip() != '':
-            dat_pieces = c.split(":")
-            if dat_pieces[1].strip() != "":
-                print c
-                f.write(c)
-                f.write("\n")
-                detected[dat_pieces[0].strip()] = dat_pieces[1].strip() 
-    f.close()
-sleep(1)
-if options.multiClone:
-    for k in detected.keys():
-        v = detected[k]
-        print "Would you like to try cloning a %s card with value %s" % (k,v)
-        ans = raw_input("[y/n]? ")
-        if ans.upper()[0] == "Y":
+    
+def main():
+    result, data=rfidler.command('autotag ')
+    detected = {}
+    with open(options.outFile,'w') as f:
+        for c in data:
+            if c.strip() != '':
+                dat_pieces = c.split(":")
+                if dat_pieces[1].strip() != "":
+                    print c
+                    f.write(c)
+                    f.write("\n")
+                    detected[dat_pieces[0].strip()] = dat_pieces[1].strip() 
+        f.close()
+    sleep(1)
+    if options.multiClone:
+        for k in detected.keys():
+            v = detected[k]
+            print "Would you like to try cloning a %s card with value %s" % (k,v)
+            ans = raw_input("[y/n]? ")
+            if ans.upper()[0] == "Y":
 
-            sleep(2)
-            rfidler.command('set tag %s' % k)
-            sleep(2)
-            rfidler.command('encode %s %s' % (k,v))
-            sleep(2)
-            result, data=rfidler.command('vtag')
-            loaded = dict_output(data)
-            print "VTAG Data Stored"
-            for field in loaded.keys():
-                print "\t%s: %s" % (field,loaded[field])
-            print "\n"
-            raw_input("Place new card on Antenna and press [Enter] ")
-            result, data=rfidler.command('clone')
-            print result," : ",data
-        else:
-            print "Skipping %s, you can still try to clone this later from the file." % k
-        
+                sleep(2)
+                rfidler.command('set tag %s' % k)
+                sleep(2)
+                rfidler.command('encode %s %s' % (k,v))
+                sleep(2)
+                result, data=rfidler.command('vtag')
+                loaded = dict_output(data)
+                print "VTAG Data Stored"
+                for field in loaded.keys():
+                    print "\t%s: %s" % (field,loaded[field])
+                print "\n"
+                raw_input("Place new card on Antenna and press [Enter] ")
+                result, data=rfidler.command('clone')
+                print result," : ",data
+            else:
+                print "Skipping %s, you can still try to clone this later from the file." % k
+
+if on_pi:
+    capture_btn = 24 # Bring to ground to start a capture stream
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(capture_btn, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    while True:
+        input_state = GPIO.input(capture_btn)
+        if input_state == False:
+            main()
+            
+else: 
+    main()
